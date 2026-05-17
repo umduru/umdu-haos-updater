01922b309e77a411076b5e9b42ff0fe56c5e4a3f
dusikasss <dusikasss26@mail.ru>
2026-05-17 12:16:32 +0300

    Use regional mirrors for addon build

--- tests/test_addon_packaging.py
+++ tests/test_addon_packaging.py
@@ -24,6 +24,18 @@ def test_dockerfile_accepts_supervisor_build_args_as_metadata_only():
     assert not re.search(r"FROM\s+.*BUILD_(VERSION|ARCH)", dockerfile)
 
 
+def test_dockerfile_uses_regional_package_mirrors():
+    dockerfile = (ADDON_DIR / "Dockerfile").read_text(encoding="utf-8")
+
+    assert "dl-cdn.alpinelinux.org" not in dockerfile
+    assert 'ARG ALPINE_MIRROR="https://mirror.yandex.ru/mirrors/alpine"' in dockerfile
+    assert "${ALPINE_MIRROR}/v3.23/main" in dockerfile
+    assert "${ALPINE_MIRROR}/v3.23/community" in dockerfile
+    assert '--repository="${ALPINE_MIRROR}/edge/testing"' in dockerfile
+    assert 'ARG PIP_INDEX_URL="https://mirror.yandex.ru/pypi/web/simple"' in dockerfile
+    assert '--index-url "${PIP_INDEX_URL}"' in dockerfile
+
+
 def test_addon_config_supports_aarch64_without_ingress_or_extra_privileges():
     config = (ADDON_DIR / "config.yaml").read_text(encoding="utf-8")
 
